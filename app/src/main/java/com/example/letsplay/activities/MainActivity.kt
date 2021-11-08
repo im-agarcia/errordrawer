935c36d5d325229694320ebe48c1911713db04eb
im-agarcia <im.agarcia@outlook.com>
2021-11-08 02:54:47 -0300

    codigo

---
+++ app/src/main/java/com/example/letsplay/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.letsplay.activities
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.letsplay.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        
+
+    }
+}
